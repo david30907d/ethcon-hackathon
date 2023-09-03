6329601b9c213d4beb535c083bfb3f44d6892782
davidtnfsh <davidtnfsh@gmail.com>
2023-09-03 11:05:12 +0900

    [feat]: remove unused tables from template

--- pages/views/Rebalancer.jsx
+++ pages/views/Rebalancer.jsx
@@ -128,34 +128,6 @@ const RebalancerWidget = ({ address }) => {
             </div>
           </Row>
         </Col>
-        <Col xl={14} md={24} xs={24} align="center">
-          <Row gutter={[30, 20]} align="center">
-            <Col xl={12} md={24} xs={24} align="left">
-              <TopNLowestAprPools
-                wording="TopN Lowest APR Pools"
-                topNData={topNLowestAprPools}
-                portfolioApr={portfolioApr}
-                windowHeight={300}
-              />
-            </Col>
-            <Col xl={12} md={24} xs={24} align="left">
-              <SuggestionsForLPTokens
-                wording="Better Pool for LP Tokens"
-                topNData={topNPoolConsistOfSameLpToken}
-                portfolioApr={portfolioApr}
-                windowHeight={300}
-              />
-            </Col>
-            <Col span={24} align="left">
-              <SuggestionsForBetterStableCoins
-                wording="Better Stable Coin Pools"
-                topNData={topNStableCoins}
-                portfolioApr={portfolioApr}
-                windowHeight={300}
-              />
-            </Col>
-          </Row>
-        </Col>
       </Row>
     </>
   );
